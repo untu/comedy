7dec00ced75d449341181a6b10a5a9b9648c08b6
weekens <weekens@gmail.com>
2016-04-19 15:28:34 +0300

    (saymon) data-server-cluster: Round robin balancer actor draft.

---
+++ standard/round-robin-balancer-actor.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var LocalActor = require('../local-actor.js');
+var _ = require('underscore');
+var P = require('bluebird');
+
+/**
+ * Actor that distributes incoming messages between it's children
+ * using round-robin strategy.
+ */
+class RoundRobinBalancerActor extends LocalActor {
+  /**
+   * @param {ActorSystem} system Actor system.
+   * @param {Actor|null} parent Actor parent or null, if it's a root actor.
+   * @param {Object} behaviour Actor behaviour definition.
+   */
+  constructor(system, parent, behaviour) {
+    super(system, parent, behaviour);
+
+    this.nextIdx = 0;
+  }
+
+  send0() {
+    return this._forward('send', _.toArray(arguments));
+  }
+
+  sendAndReceive0() {
+    return this._forward('sendAndReceive', _.toArray(arguments));
+  }
+
+  /**
+   * Forwards message to one of load-balanced children.
+   *
+   * @param {String} methodName Name of the method to use for sending.
+   * @param {Array} args Arguments for send method.
+   * @returns {P} Operation promise.
+   * @private
+   */
+  _forward(methodName, args) {
+    var children = this._children();
+
+    if (_.isEmpty(children)) {
+      return P.resolve().throw(new Error('No children to forward message to.'));
+    }
+
+    if (this.nextIdx > children.length - 1) {
+      this.nextIdx = 0;
+    }
+
+    var currentChild = children[this.nextIdx++];
+
+    return currentChild[methodName].apply(currentChild, args);
+  }
+}
+
+module.exports = RoundRobinBalancerActor;
