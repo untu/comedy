39c60e468bcb68c2bfe94974286795801771f8e4
weekens <weekens@gmail.com>
2017-05-28 10:09:10 +0300

    Added split message MessageSocket test.

--- test/test-message-socket.js
+++ test/test-message-socket.js
@@ -11,6 +11,7 @@ var MessageSocket = require('../lib/net/message-socket.js');
 var net = require('net');
 var expect = require('chai').expect;
 var P = require('bluebird');
+var _ = require('underscore');
 
 var server;
 var client;
@@ -35,7 +36,7 @@ describe('MessageSocket', function() {
     server = net.createServer(socket => {
       var serverMessageSocket = new MessageSocket(socket);
 
-      serverMessageSocket.on('message', msg => {
+      serverMessageSocket.once('message', msg => {
         expect(msg).to.be.deep.equal({ text: 'Hello world!' });
 
         done();
@@ -53,7 +54,50 @@ describe('MessageSocket', function() {
     client.on('error', done);
   });
 
-  it('should correctly receive 1 message in 2 chunks');
+  it('should correctly receive 1 message in 2 chunks', done => {
+    server = net.createServer(socket => {
+      var serverMessageSocket = new MessageSocket(socket);
+
+      serverMessageSocket.once('message', msg => {
+        expect(msg).to.be.deep.equal({ text: 'Hello world!' });
+
+        done();
+      });
+    });
+
+    server.listen(6363);
+    server.on('error', done);
+
+    client = net.createConnection(6363, () => {
+      var clientMessageSocket = new MessageSocket(client);
+      var packet = clientMessageSocket.makePacket({ text: 'Hello world!' });
+      var chunks = splitBuffer(packet, 2);
+
+      _.each(chunks, chunk => client.write(chunk));
+    });
+    client.on('error', done);
+  });
 
   it('should correctly receive 2 messages in 3 chunks');
 });
+
+/**
+ * Splits a given buffer into a given number of chunks
+ * with nearly equal length.
+ *
+ * @param {Buffer} buf Buffer to split.
+ * @param {Number} nChunks Number of chunks.
+ * @returns {Buffer[]} Array of resulting chunks.
+ */
+function splitBuffer(buf, nChunks) {
+  var chunkLength = Math.trunc(buf.length, nChunks);
+  var lastChunkLength = chunkLength + buf.length % nChunks;
+
+  return _.times(nChunks, i => {
+    var len = i == buf.length - 1 ? lastChunkLength : chunkLength;
+    var chunk = Buffer.alloc(len);
+    buf.copy(chunk, 0, chunkLength * i, len);
+
+    return buf;
+  });
+}
