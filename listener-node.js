48906fcd14bc86bf8efeb0266f18f4fa550e6c16
weekens <weekens@gmail.com>
2017-06-12 16:59:03 +0300

    Added remote actor documentation.

--- listener-node.js
+++ listener-node.js
@@ -1,8 +1,11 @@
-#!/usr/bin/env node
-
 /*
- * ~ Copyright (c) 2014-2016 ROSSINNO, LTD.
+ * Copyright (c) 2016 Untu, Inc.
+ * This code is licensed under Eclipse Public License - v 1.0.
+ * The full license text can be found in LICENSE.txt file and
+ * on the Eclipse official site (https://www.eclipse.org/legal/epl-v10.html).
  */
+
+#!/usr/bin/env node
 
 'use strict';
 
