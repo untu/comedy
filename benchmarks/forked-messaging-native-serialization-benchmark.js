bd2cf0abdf112891d974be1a89c1d2b5e4364ba5
weekens <weekens@gmail.com>
2017-06-20 12:55:28 +0300

    Updated copyright.

--- benchmarks/forked-messaging-native-serialization-benchmark.js
+++ benchmarks/forked-messaging-native-serialization-benchmark.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Untu, Inc.
+ * Copyright (c) 2016-2017 Untu, Inc.
  * This code is licensed under Eclipse Public License - v 1.0.
  * The full license text can be found in LICENSE.txt file and
  * on the Eclipse official site (https://www.eclipse.org/legal/epl-v10.html).
