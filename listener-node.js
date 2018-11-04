9d71fd9829648dfa691f8d0bbd31668cc36033a2
weekens <weekens@gmail.com>
2018-11-04 09:44:16 +0300

    Updated copyright.

--- listener-node.js
+++ listener-node.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 /*
- * Copyright (c) 2016-2017 Untu, Inc.
+ * Copyright (c) 2016-2018 Untu, Inc.
  * This code is licensed under Eclipse Public License - v 1.0.
  * The full license text can be found in LICENSE.txt file and
  * on the Eclipse official site (https://www.eclipse.org/legal/epl-v10.html).
